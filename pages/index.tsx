6a5beaea8087b0a27e6d27946b27184f6cc4d758
Lucas Santos <lucashfreitas1@gmail.com>
2022-12-20 23:48:01 +1300

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,117 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import { useMemo, useState } from "react";
+import Layout from "../components/Layout";
+import type { ColumnDef, ColumnFiltersState } from "@tanstack/react-table";
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+type User = {
+  name: string;
+};
 
-export default IndexPage
+const IndexPage = () => {
+  const [globalFilter, setGlobalFilter] = useState("");
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+  const data: User[] = [
+    {
+      name: "John",
+    },
+    {
+      name: "Jane",
+    },
+  ];
+
+  const columns = useMemo<Array<ColumnDef<User>>>(
+    () => [
+      {
+        accessorKey: "name",
+        header: () => <span>name</span>,
+        cell: (info) => info.getValue(),
+        footer: (info) => info.column.id,
+      },
+    ],
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  const table = useReactTable({
+    data,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    state: {
+      columnFilters,
+      globalFilter,
+    },
+    onColumnFiltersChange: setColumnFilters,
+    onGlobalFilterChange: setGlobalFilter,
+    columns: columns,
+    debugTable: true,
+    debugHeaders: true,
+    debugColumns: true,
+  });
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Table</h1>
+      <div className="mt-2 flex flex-col">
+        <div className="sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+              <>
+                <table className="min-w-full divide-y divide-gray-300">
+                  <thead className="bg-gray-50">
+                    {table.getHeaderGroups().map((headerGroup) => (
+                      <tr key={headerGroup.id}>
+                        {headerGroup.headers.map((header) => (
+                          <th
+                            className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8"
+                            key={header.id}
+                          >
+                            {header.isPlaceholder
+                              ? null
+                              : flexRender(
+                                  header.column.columnDef.header,
+                                  header.getContext()
+                                )}
+                          </th>
+                        ))}
+                      </tr>
+                    ))}
+                  </thead>
+                  <tbody className="divide-y divide-gray-200 bg-white">
+                    {table.getRowModel().rows.map((row) => (
+                      <tr key={row.id}>
+                        {row.getVisibleCells().map((cell, index) => (
+                          <td
+                            className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8"
+                            key={`${index}-${cell.id}`}
+                          >
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                            )}
+                          </td>
+                        ))}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </>
+            </div>
+          </div>
+        </div>
+      </div>
+      <p>
+        <Link href="/about">About</Link>
+      </p>
+    </Layout>
+  );
+};
+
+export default IndexPage;
